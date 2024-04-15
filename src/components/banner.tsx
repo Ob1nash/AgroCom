893281657bc323c408e5ae709c73997d7a81d557
Abinash Kalita <kalitaabi123@gmail.com>
2024-04-15 13:08:51 +0530

    image bugs fixed

--- src/components/banner.tsx
+++ src/components/banner.tsx
@@ -3,9 +3,9 @@ import { motion } from 'framer-motion';
 
 // Define the array of slides with images and headings
 const slides = [
-    { image: 'public/images/featured.png', heading: 'Add a product to cart or wishlist for later' },
-    { image: 'public/images/cart.png', heading: 'View Cart' },
-    { image: 'public/images/checkout.png', heading: 'Click on Buy Now to Confirm purchase' },
+    { image: '/images/featured.png', heading: 'Add a product to cart or wishlist for later' },
+    { image: '/images/cart.png', heading: 'View Cart' },
+    { image: '/images/checkout.png', heading: 'Click on Buy Now to Confirm purchase' },
 ];
 
 const Slider = () => {
